22d8d2f5bceed1e19c667d1e96a1c858ffeed842
Troy Dawson <troyd9216@gmail.com>
2020-07-20 04:43:01 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,8 +81,9 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
-
+      
+      }
+     programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 end
 
 def changing_alan
@@ -103,7 +105,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -124,6 +128,7 @@ def adding_to_dennis
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
-
+}
+programmer_hash[:dennis_ritchie][:languages][1]=["Assembly"]
+programmer_hash
 end
