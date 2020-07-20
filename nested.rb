4845db62efefa8e0df7365bf2e3d9ad7f9290165
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-20 20:49:40 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
 
 def hopper
-	programmer_hash = 
- 		{
+	programmer_hash = {
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -15,9 +14,14 @@ def hopper
         :languages => ["C"]
       }
     }
-
+   
+  programmer_hash = {
+    :known_for => "COBOL",
+    :languages => ["COBOL", "FORTRAN"]
+  }
+    return programmer_hash
 end
-
+   
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
@@ -36,7 +40,10 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash = "Object Orientation"
+    
+  return programmer_hash
 end
 
 def dennis_ritchies_language
@@ -56,6 +63,8 @@ def dennis_ritchies_language
       }
     }
 
+programmer_hash = "C"
+return programmer_hash
 end
 
 def adding_matz
@@ -81,7 +90,15 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    
+ programmer_hash = {
+   :yukihiro_matsumoto => {
+     :known_for => "Ruby",
+    :languages => ["LISP"]
+   }
+ }
+   
+ return programmer_hash
 end
 
 def changing_alan
@@ -104,6 +121,12 @@ def changing_alan
       }
     }
 
+programmer_hash = {
+  :alan_kay => {
+    :known_for => "GUI"
+  }
+}
+return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +148,12 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash = {
+      :dennis_ritchie => {
+        :languages => ["C", "Assembly"]
+      }
+    }
+    return programmer_hash
 
 end
